f78a105371a0bcfa8750f6c7bb513677cc0b5001
James Contant <>
2024-02-06 18:21:33 -0800

    before auth

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,72 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from "react-native";
+import { Text, View } from "@/components/Themed";
+import { Link } from "expo-router";
+// import { useAuth0 } from "react-native-auth0";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+export default function HomePage() {
+//   const LoginButton = () => {
+//     const { authorize } = useAuth0();
 
-export default function TabOneScreen() {
+//     const onPress = async () => {
+//       try {
+//         await authorize();
+//       } catch (e) {
+//         console.log(e);
+//       }
+//     };
+
+//     return <Button onPress={onPress} title="Log in" />;
+//   };
+
+//   const LogoutButton = () => {
+//     const { clearSession } = useAuth0();
+
+//     const onPress = async () => {
+//       try {
+//         await clearSession();
+//       } catch (e) {
+//         console.log(e);
+//       }
+//     };
+
+//     return <Button onPress={onPress} title="Log out" />;
+//   };
+
+  // const Profile = () => {
+  //   const { user, error } = useAuth0();
+
+  //   return (
+  //     <>
+  //       {user && <Text>Logged in as {user.name}</Text>}
+  //       {!user && <Text>Not logged in</Text>}
+  //       {error && <Text>{error.message}</Text>}
+  //     </>
+  //   );
+  // };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={styles.row}>
+        {/* <Link href={`/plans/`}>
+          <Text>Go To All Workout Plans</Text>
+        </Link> */}
+        {/* <LoginButton />
+        <LogoutButton />
+        <Profile /> */}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "stretch",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  row: {
+    alignSelf: "stretch",
+    padding: 10,
   },
 });
